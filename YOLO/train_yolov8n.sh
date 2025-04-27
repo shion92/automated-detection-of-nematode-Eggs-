0cd59ad1207cd8ed8aa8af5e7643adef6b5bf2f7
shion92 <shionshine@gmail.com>
2025-04-27 16:25:41 +1200

    yolo comparison

--- YOLO/train_yolov8n.sh
+++ YOLO/train_yolov8n.sh
@@ -1,14 +1,11 @@
-# === Train YOLOv8s on your annotated dataset (optimised settings) ===
-# === Make sure to run this script in the YOLO directory ===
+# === Train YOLOv8n on your annotated dataset (optimised settings) ===
+# Make sure to run this script from your YOLO folder
 cd /Users/shion/Desktop/COMP693/automated-detection-of-nematode-Eggs-/YOLO
 
-yolo task=detect mode=train model=YOLO/yolov8s.pt data=data.yaml epochs=200 imgsz=608 batch=16 lr0=0.01 patience=30 degrees=10 translate=0.1 scale=0.2 shear=2 perspective=0.0005 mosaic=1.0 hsv_h=0.015 hsv_s=0.7 hsv_v=0.4 project=YOLO name=nematode_yolov8s_train
+yolo task=detect mode=train model=yolov8n.pt data=data.yaml epochs=200 imgsz=608 batch=16 lr0=0.01 patience=30 degrees=10 translate=0.1 scale=0.2 shear=2 perspective=0.0005 mosaic=1.0 hsv_h=0.015 hsv_s=0.7 hsv_v=0.4 project=YOLO name=nematode_yolov8n_train
 
 # === Evaluate the best.pt after training ===
-yolo task=detect mode=val model=nematode_yolov8s_train2/weights/best.pt data=data.yaml project=../YOLO/nematode_yolov8s name=val save_json=True verbose=True
-
+yolo task=detect mode=val model=nematode_yolov8n_train/weights/best.pt data=data.yaml project=nematode_yolov8n name=val save_json=True verbose=True save_txt=True
 
 # === Predict on new test images ===
-yolo task=detect mode=predict model=nematode_yolov8s_train2/weights/best.pt source=../dataset/test/images project=../YOLO/nematode_yolov8s name=test save_json=True save_txt=True save_conf=True verbose=True
-
-
+yolo task=detect mode=predict model=nematode_yolov8n_train/weights/best.pt source=../dataset/test/images project=nematode_yolov8n name=test save_json=True save_txt=True save_conf=True verbose=True
--- labelling.py
+++ labelling.py
@@ -1,9 +1,20 @@
-
+# Use LabelImg for Interactive Adjustment
 # labeling images with labelImg
 pip install labelImg
 labelImg
 
-
+# Steps to Adjust Bounding Boxes in LabelImg
+# Open LabelImg.
+# Click "Open Dir", and select the folder containing your images.
+# Click "Change Save Dir", and select the folder containing the .txt annotations.
+# Click "Open Next Image" (or manually select an image).
+# The auto-detected bounding boxes should appear.
+# Adjust bounding boxes manually:
+# Click and drag to resize or move boxes.
+# Delete incorrect boxes using the "Delete" button.
+# Add new boxes using the "Create RectBox" button.
+# Click "Save" after each image adjustment.
+# Move to the next image and repeat.
 
 # activate tensorboard
 pip install tensorboard
